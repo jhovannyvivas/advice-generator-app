5a03e707e45cfd429ab4647f52dc244884049de8
Jhovanny Vivas <jhovannyvivas@gmail.com>
2022-06-25 00:45:53 -0500

    ajustes finales

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ let dice_element = document.getElementById('dice');
 let advice =  "";
 let number;
 let error = false;
+let permite_click = true;
 
 const downloadDatos = async () => {
+    permite_click = false;
     await fetch('https://api.adviceslip.com/advice')
     .then(resp=> resp.json())
     .then(data=> {
@@ -23,10 +25,33 @@ const downloadDatos = async () => {
         error = false;
     } else {
         console.log(advice);
-        advice_element.innerText = advice;
-        number_element.innerText = number;  
+        advice_element.innerText = `"${advice}"`;
+        number_element.innerText = number;
+        permite_click = true;
     }
 };
+
+const cambiarImagen = () => {
+    let width_viewport = document.documentElement.clientWidth;
+    let imagen_mobile = document.getElementById('mobile_image');
+    let destop_image = document.getElementById('destop_image');
+
+    if(width_viewport > 375) {
+        destop_image.classList.remove("ocultar_imagen");
+        imagen_mobile.classList.add("ocultar_imagen");
+        console.log('cambio');
+    } else {
+        imagen_mobile.classList.remove("ocultar_imagen");
+        destop_image.classList.add("ocultar_imagen");
+        console.log('cambio');
+    }
+
+
+}
+
+window.onresize = cambiarImagen;
+
+
 
 
 document.addEventListener("DOMContentLoaded", downloadDatos);
